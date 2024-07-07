41d5d6d97ff312f1230127d4295aa1b8d558bc16
motalebjomadar <motalabjomadar@gmail.com>
2024-07-07 14:05:43 +0800

    password validation with react form and apply on change attributes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PwdValidation from './components/PwdValidation'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <PwdValidation></PwdValidation>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PwdValidation.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+
+const PwdValidation = () => {
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(password.length < 6 ){
+            setError("Password must be 6 character or longer");
+        }
+        else{
+            setError('');
+        }
+        console.log(name, email, password);
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    }
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleNameChange} type="text" name="name" required/>
+                <br/><br/>
+                <input onChange={handleEmailChange} type="text" name="email" required/>
+                <br/><br/>
+                <input onChange={handlePasswordChange} type="text" name="password" required/>
+                <br/><br/>
+                <input type="submit" value="Submit"/>
+
+                {
+                    error && <p>Wrong Password!!!</p>
+                }
+            </form>
+        </div>
+    );
+};
+
+export default PwdValidation;
